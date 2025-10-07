59833cc8a3d6a65a794a89c9033cb6a4487fc8bf
Zachareee <zacharytanhx@yahoo.com>
2025-10-07 16:37:26 +0800

    Nmixx change up dockerfile

--- next.config.js
+++ next.config.js
@@ -2,11 +2,8 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+    /* config options here */
+    output: process.env.NODE_ENV === "production" ? "standalone" : undefined
 };
 
-module.exports = {
-  // ... rest of the configuration.
-  output: "standalone",
-};
-
+export default nextConfig
